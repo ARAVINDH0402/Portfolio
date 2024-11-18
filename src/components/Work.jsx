f7b0fd4673b7aceb45d9520916a7693065319678
ARAVINDH0402 <saravindh04@gmail.com>
2024-11-18 17:37:19 +0530

    change in work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,34 +13,10 @@ const works = [
       projectLink: 'https://github.com/ARAVINDH0402/Login-Register-using-MERN-stack'
     },
     {
-      imgSrc: './images/project-2.jpg',
-      title: 'Free stock photo app',
-      tags: ['API', 'SPA'],
-      projectLink: 'https://pixstock-official.vercel.app/'
-    },
-    {
-      imgSrc: './images/project-3.jpg',
-      title: 'Recipe app',
-      tags: ['Development', 'API'],
-      projectLink: ''
-    },
-    {
-      imgSrc: './images/project-4.jpg',
-      title: 'Real state website',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee-org/wealthome'
-    },
-    {
-      imgSrc: './images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-    },
-    {
-      imgSrc: './images/project-6.jpg',
-      title: 'vCard Personal portfolio',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+      imgSrc: './images/portfolio thumbnail.jpg',
+      title: 'My portfolio',
+      tags: ['Web-design', 'Portfolio'],
+      projectLink: 'https://github.com/ARAVINDH0402/Portfolio'
     },
   ];
 
